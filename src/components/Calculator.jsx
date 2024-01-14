659cf5b788315ccbe90f99abdcaae23fa605606b
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-01-14 00:07:00 -0800

    Add current number state

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,18 +1,49 @@
+import { useState } from 'react';
+
 import { calculatorButtons } from '../globals/calculator-button-data';
 import Button from './CalculatorButtons';
 
 function Calculator() {
+    const [currentNum, setCurrentNum] = useState("0");   // Current value user input into calculator
+    const [previousNum, setPreviousNum] = useState(""); 
+
+    const maxDigits = 13;
+
+    function onBtnClick(e) {
+        const btnType = e.target.dataset.action;
+        const btnValue = e.target.value;
+
+        if (btnType === "number") {
+            setCurrentNum(currentNum => {   // Update number
+                if (currentNum === "0") currentNum = "";   // Delete placeholder "0"        
+                return (
+                    (currentNum.length < maxDigits)   // Limit number to max digits
+                        ? currentNum + btnValue
+                        : currentNum
+                );
+            });
+        }
+    }
+
+    function renderDisplay(updatedNumber) {
+        if (currentNum === 0) {
+            return updatedNumber;
+        }
+    }
+
     function renderButtons() {
         return (
             calculatorButtons.map((btnData) => {
-                return (<Button btnData={btnData} />)
+                return (<Button btnData={btnData} onBtnClick={onBtnClick} />)
             })
         )
     }
 
     return (
         <div className="calculator">
-            <div className="display"></div>
+            <div className="display">
+                {currentNum}
+            </div>
             <div className="buttons">
                 {renderButtons()}
             </div>
--- src/components/CalculatorButtons.jsx
+++ src/components/CalculatorButtons.jsx
@@ -1,11 +1,11 @@
-function Button({ btnData }) {
+function Button({ btnData, onBtnClick }) {
     return (
         <button 
             type="button"
             className={btnData.className}
             value={btnData.value}
             data-action={btnData.type}
-
+            onClick={onBtnClick}
         >
             {btnData.text}
         </button>
