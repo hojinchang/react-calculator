0621575db8938456e0f3d41fca0d1e8358621471
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-01-13 22:37:48 -0800

    Add mathematical operation functions

---
+++ src/utils/operations.js
@@ -0,0 +1,36 @@
+// Add 2 numbers together
+function add(x, y) {
+    return x + y;
+}
+
+// Subtract second number from first numner
+function subtract(x, y) {
+    return x - y;
+}
+
+// Multiply 2 numbers together
+function multiply(x, y) {
+    return x * y;
+}
+
+// Divide second number from first number
+function divide(x, y) {
+    return x / y;
+}
+
+// Square root the number
+function squareRoot(x) {
+    return Math.sqrt(x);
+}
+
+// Convert number into a percentage
+function percent(x) {
+    return x / 100;
+}
+
+// Reverse polarity of a number
+function polarity(x) {
+    return x * -1;
+}
+
+export { add, subtract, multiply, divide, squareRoot, percent, polarity };
