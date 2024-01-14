6ff2aa86fa8877bf9b69d89bf05b16e0c9dba9b3
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-01-14 02:08:47 -0800

    Refractor operations to convert strings into numbers first

--- src/utils/operations.js
+++ src/utils/operations.js
@@ -1,36 +1,36 @@
 // Add 2 numbers together
 function add(x, y) {
-    return x + y;
+    return Number(x) + Number(y);
 }
 
 // Subtract second number from first numner
 function subtract(x, y) {
-    return x - y;
+    return Number(x) - Number(y);
 }
 
 // Multiply 2 numbers together
 function multiply(x, y) {
-    return x * y;
+    return Number(x) * Number(y);
 }
 
 // Divide second number from first number
 function divide(x, y) {
-    return x / y;
+    return Number(x) / Number(y);
 }
 
 // Square root the number
 function squareRoot(x) {
-    return Math.sqrt(x);
+    return Math.sqrt(Number(x));
 }
 
 // Convert number into a percentage
 function percent(x) {
-    return x / 100;
+    return Number(x) / 100;
 }
 
 // Reverse polarity of a number
 function polarity(x) {
-    return x * -1;
+    return Number(x) * -1;
 }
 
 export { add, subtract, multiply, divide, squareRoot, percent, polarity };
